19e267ce2cdc284f443c9135c8aee4a425aeee89
Jennifer Okwara <jokwara03@gmail.com>
2018-07-31 17:04:57 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  gets '/' do 
+    "Hello World"
+  end
+  
+  #localhost/name
+  gets '/name' do
+    "My name is Jen"
+  end
+
+
 end
